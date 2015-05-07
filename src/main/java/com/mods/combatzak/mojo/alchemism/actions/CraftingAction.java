fabaf1b3d2ad82f2adf4dd054464a8e60d9af682
CombatZAK <combatzak@gmail.com>
2015-05-06 23:19:10 -0400

    Additions
    Added recipes IIngredient interface and Crafting Action functionality;
    may need to revise

--- src/main/java/com/mods/combatzak/mojo/alchemism/actions/CraftingAction.java
+++ src/main/java/com/mods/combatzak/mojo/alchemism/actions/CraftingAction.java
@@ -1,6 +1,10 @@
 package com.mods.combatzak.mojo.alchemism.actions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.mods.combatzak.mojo.MojoAction;
+import com.mods.combatzak.mojo.alchemism.recipes.IIngredient;
 
 /**
  * An action that affects standard crafting
@@ -8,5 +12,12 @@ import com.mods.combatzak.mojo.MojoAction;
  *
  */
 public abstract class CraftingAction extends MojoAction {
-
+	protected List<IIngredient> inputs = new ArrayList<IIngredient>(); //series of ingredients for the recipe
+	
+	/**
+	 * Gets the inputs for the crafting recipe
+	 * 
+	 * @return
+	 */
+	protected List<IIngredient> getInputs() { return this.inputs; }
 }
---
+++ src/main/java/com/mods/combatzak/mojo/alchemism/recipes/IIngredient.java
@@ -0,0 +1,16 @@
+package com.mods.combatzak.mojo.alchemism.recipes;
+
+/**
+ * Represents a recipe ingredient
+ * 
+ * @author CombatZAK
+ *
+ */
+public interface IIngredient {
+	
+	/**
+	 * Retrieves the forge ingredient object for the recipe
+	 * @return Ingredient for recipe
+	 */
+	public Object getIngredient();
+}
