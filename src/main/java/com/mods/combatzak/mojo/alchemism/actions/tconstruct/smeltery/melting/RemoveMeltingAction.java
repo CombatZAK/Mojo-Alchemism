affa0decc59a2be58b634d127e82f2c03bbd2e99
CombatZAK <combatzak@gmail.com>
2015-06-03 17:34:54 -0400

    Reconcile build
    Fixing checkin (again)

--- src/main/java/com/mods/combatzak/mojo/alchemism/actions/tconstruct/smeltery/melting/RemoveMeltingAction.java
+++ src/main/java/com/mods/combatzak/mojo/alchemism/actions/tconstruct/smeltery/melting/RemoveMeltingAction.java
@@ -1,5 +1,11 @@
 package com.mods.combatzak.mojo.alchemism.actions.tconstruct.smeltery.melting;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import tconstruct.library.crafting.Smeltery;
+import mantle.utils.ItemMetaWrapper;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
 
@@ -21,7 +27,7 @@ public class RemoveMeltingAction extends MeltingAction {
 	}
 	
 	/**
-	 * Creates a new RemoveMeltingActionInstance
+	 * Creates a new RemoveMeltingAction instance
 	 * 
 	 * @param input input filter
 	 */
@@ -29,10 +35,53 @@ public class RemoveMeltingAction extends MeltingAction {
 		this(input, null);
 	}
 	
+	/**
+	 * Creates a new RemoveMeltingAction instance
+	 * 
+	 * @param output output filter
+	 */
+	public RemoveMeltingAction(FluidStack output) {
+		this(null, output);
+	}
+	
+	/**
+	 * Default constructor
+	 */
+	public RemoveMeltingAction() {
+		this(null, null);
+	}
+	
+	/**
+	 * Applies the action, removing any melting recipes that match the filter
+	 */
 	@Override
-	public boolean apply() {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean apply() throws IllegalStateException {
+		if (input == null && output == null) throw new IllegalStateException("Cannot remove melting recipes without filter"); //null test
+		
+		Map<ItemMetaWrapper, FluidStack> meltList = Smeltery.getSmeltingList(); //get the list of i/o
+		Map<ItemMetaWrapper, Integer> temperatureList = Smeltery.getTemperatureList(); //get the list of temperatures
+		Map<ItemMetaWrapper, ItemStack> renderList = Smeltery.getRenderIndex(); //get the list of renders
+		
+		List<ItemMetaWrapper> matchKeys = new ArrayList<ItemMetaWrapper>();
+		for (Map.Entry<ItemMetaWrapper, FluidStack> entry : Smeltery.getSmeltingList().entrySet()) { //go through all the IO recipes
+			if (isPatternMatch(entry)) //match to the i/o pattern
+				matchKeys.add(entry.getKey());
+		}
+		
+		for (ItemMetaWrapper match : matchKeys) { //go through all the matches
+			//remove from all three lists
+			meltList.remove(match);
+			temperatureList.remove(match);
+			renderList.remove(match);
+		}
+		
+		setIsApplied(true); //set the applied flag
+		return true;
 	}
 
+	private boolean isPatternMatch(Map.Entry<ItemMetaWrapper, FluidStack> recipe) {
+		if (recipe == null) return false;
+		
+		boolean inputMatches = input == null || recipe.getKey().item == input.getItem()
+	}
 }
