97347fc3234f8725e88667e0fdc468f8711b480a
CombatZAK <combatzak@gmail.com>
2018-04-19 20:06:03 -0400

    Hard wipe the mess that came before

--- src/main/java/com/mods/combatzak/mojo/alchemism/nei/tooltip/helpers/xu/GeneratorFuelTooltipHelper.java
+++
@@ -1,7 +0,0 @@
-package com.mods.combatzak.mojo.alchemism.nei.tooltip.helpers.xu;
-
-import com.mods.combatzak.mojo.alchemism.nei.tooltip.helpers.FuelTooltipHelper;
-
-public abstract class GeneratorFuelTooltipHelper extends FuelTooltipHelper {
-
-}
