18542bc27c1bb8d200254fe99eedeeaf4f47267f
CombatZAK <combatzak@gmail.com>
2018-05-19 21:26:06 -0400

    Update forestry and fix squeezer again

--- src/main/java/com/mods/combatzak/mojo/alchemism/UpdateManagers/SqueezerUpdateManager.java
+++ src/main/java/com/mods/combatzak/mojo/alchemism/UpdateManagers/SqueezerUpdateManager.java
@@ -1,14 +1,18 @@
 package com.mods.combatzak.mojo.alchemism.UpdateManagers;
 
 import binnie.extrabees.items.types.EnumHoneyDrop;
+import binnie.extrabees.items.types.EnumPropolis;
+import cofh.thermalfoundation.init.TFFluids;
 import cofh.thermalfoundation.item.ItemMaterial;
 import com.mods.combatzak.mojo.alchemism.actions.forestry.squeezer.AddOrReplaceSqueezerContainerRecipeAction;
 import com.mods.combatzak.mojo.alchemism.actions.forestry.squeezer.AddOrReplaceSqueezerRecipeAction;
 import com.mods.combatzak.mojo.alchemism.actions.forestry.squeezer.ReplaceSqueezerOutputAction;
+import forestry.arboriculture.FruitProviderPod;
 import forestry.core.ModuleCore;
 import forestry.core.ModuleFluids;
 import forestry.core.fluids.Fluids;
 import mekanism.common.MekanismFluids;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraftforge.fluids.FluidStack;
@@ -31,6 +35,8 @@ public class SqueezerUpdateManager extends UpdateManager {
     private SqueezerUpdateManager() {
         updateActions.add(new AddOrReplaceSqueezerRecipeAction(10, Stream.of(EnumHoneyDrop.SEED.get(1)).collect(Collectors.toList()), ItemStack.EMPTY, 0, Fluids.SEED_OIL.getFluid(200)));
         updateActions.add(new AddOrReplaceSqueezerRecipeAction(10, Stream.of(EnumHoneyDrop.ACID.get(1)).collect(Collectors.toList()), ItemStack.EMPTY, 0, new FluidStack(MekanismFluids.SulfuricAcid.getFluid(), 200)));
+        updateActions.add(new AddOrReplaceSqueezerRecipeAction(10, Stream.of(EnumHoneyDrop.ICE.get(1)).collect(Collectors.toList()), ItemStack.EMPTY, 0, Fluids.ICE.getFluid(200)));
+        updateActions.add(new AddOrReplaceSqueezerRecipeAction(20, Stream.of(EnumPropolis.FUEL.get(1)).collect(Collectors.toList()), ItemStack.EMPTY, 0, new FluidStack(TFFluids.fluidFuel, 500)));
         updateActions.add(new AddOrReplaceSqueezerContainerRecipeAction(20, ModuleFluids.getItems().canEmpty.getItemStack(), ItemMaterial.ingotTin, 5));
     }
 }
--- src/main/java/com/mods/combatzak/mojo/alchemism/actions/forestry/squeezer/AddOrReplaceSqueezerContainerRecipeAction.java
+++ src/main/java/com/mods/combatzak/mojo/alchemism/actions/forestry/squeezer/AddOrReplaceSqueezerContainerRecipeAction.java
@@ -1,8 +1,11 @@
 package com.mods.combatzak.mojo.alchemism.actions.forestry.squeezer;
 
+import com.mods.combatzak.mojo.alchemism.helpers.ItemStackHelper;
 import forestry.api.recipes.RecipeManagers;
+import forestry.factory.recipes.SqueezerRecipeManager;
 import net.minecraft.item.ItemStack;
 
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -18,6 +21,18 @@ public class AddOrReplaceSqueezerContainerRecipeAction extends AddOrReplaceSquee
 
     @Override
     public boolean apply() {
+        ItemStack match = null;
+        for (ItemStack inputKey : SqueezerRecipeManager.containerRecipes.keySet()) {
+            if (ItemStackHelper.isDirectMatch(inputs.get(0), inputKey)) {
+                match = inputKey;
+                break;
+            }
+        }
+
+        if (match != null) {
+            SqueezerRecipeManager.containerRecipes.remove(match);
+        }
+
         RecipeManagers.squeezerManager.addContainerRecipe(time, inputs.get(0), itemOutput, itemChance / 100f);
         setApplied();
         return true;
